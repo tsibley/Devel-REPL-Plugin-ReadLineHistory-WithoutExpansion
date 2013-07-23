ad7abb96521ef3fc1855223d79349d36f76c2833
Thomas Sibley <tsibley@cpan.org>
2013-07-22 22:13:58 -0700

    milla skeleton

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Devel::REPL::Plugin::ReadLineHistory::WithoutExpansion - Blah blah blah
+
+# SYNOPSIS
+
+    use Devel::REPL::Plugin::ReadLineHistory::WithoutExpansion;
+
+# DESCRIPTION
+
+Devel::REPL::Plugin::ReadLineHistory::WithoutExpansion is
+
+# AUTHOR
+
+Thomas Sibley <tsibley@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2013- Thomas Sibley
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Devel/REPL/Plugin/ReadLineHistory/WithoutExpansion.pm
@@ -0,0 +1,39 @@
+package Devel::REPL::Plugin::ReadLineHistory::WithoutExpansion;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Devel::REPL::Plugin::ReadLineHistory::WithoutExpansion - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Devel::REPL::Plugin::ReadLineHistory::WithoutExpansion;
+
+=head1 DESCRIPTION
+
+Devel::REPL::Plugin::ReadLineHistory::WithoutExpansion is
+
+=head1 AUTHOR
+
+Thomas Sibley E<lt>tsibley@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Thomas Sibley
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
